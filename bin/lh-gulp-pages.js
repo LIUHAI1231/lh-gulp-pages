8d6d03bb7517bd555ffd1b683e4821ee1136d8e5
LIUHAI1231 <370432344@qq.com>
2020-07-08 22:25:04 +0800

    .

--- bin/lh-gulp-pages.js
+++ bin/lh-gulp-pages.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-// console.log('bin/lh-gulp-pages')
+// console.log('bin/gulp-pages')
 
 process.argv.push('--cwd')
 process.argv.push(process.cwd())
 process.argv.push('--gulpfile')
 process.argv.push(require.resolve('..'))
 
-console.log(process.argv)
+// console.log(process.argv)
 
 require('gulp/bin/gulp')
